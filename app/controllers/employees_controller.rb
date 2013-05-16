ff167fb35e2fe352d34dcff91cc43b7a1e019f44
Connor and Cody <C_C@appacademy.io>
2013-05-16 15:49:37 -0400

    implementing paperclip

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,6 +7,9 @@ class EmployeesController < ApplicationController
     end
   end
 
+  def show
+    @employee = Employee.find(params[:id])
+  end
   def new
     @employee = Employee.new
     @employee.build_profile
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ActiveRecord::Base
   attr_accessible :height, :fav_food, :fav_day, :birthday, :salary, :photo_url
 
+   has_attached_file :photo_url, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://cdn-thumbs.mumblur.com/media/allthumbs/article_default_1368073320_club-owner-sues-future-for-missing-club-appearance-cause-of-his-boo-ciara.jpg"
+
   has_one :employee
 
   validates :height, :fav_food, :birthday, :salary, :photo_url, presence: true
